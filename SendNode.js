07dc08a2df212b615634232f12677f0df582d381
Claudio Magno <clmagno22@gmail.com>
2019-05-23 10:22:26 -0300

    Oi teste

--- SendNode.js
+++ SendNode.js
@@ -3,7 +3,7 @@ var request = require("request");
 
 var options = { method: 'POST',
   url: 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution',
-  qs: { token: 'e5b63083899cbd9feb21e0716ce712ae00890a57' },
+  qs: { token: '554fgffgfgfgfrtrtr655655656rttrt' },
   headers: 
    { 
      'cache-control': 'no-cache',
@@ -40,7 +40,7 @@ headers=
      'content-type': 'multipart/form-data'  
   }
  
-request.post({url:'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=e5b63083899cbd9feb21e0716ce712ae00890a57', formData: formData,headers:headers}, function(err, httpResponse, body) {
+request.post({url:'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=554fgffgfgfgfrtrtr655655656rttrt', formData: formData,headers:headers}, function(err, httpResponse, body) {
   if (err) {
     return console.error('upload failed:', err);
   }
